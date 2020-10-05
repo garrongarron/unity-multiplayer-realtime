254ddb2fe1963df3078128cffbd0099c23d9b6bc
Federico Zacayan <garron.garron@gmail.com>
2020-10-05 11:18:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const net = require('net')
 const PORT = 3000
 var servidor = net.createServer(function (socket) {
-    socket.write(Buffer.from("conectado", "utf-8"));
+    socket.write(Buffer.from("connected (Message from SERVER)", "utf-8"));
     socket.on("close", function () {
-        console.log("DESCONECTADO");
+        console.log("Client disconnected");
     });
+    socket.on("data", function (data) {
+        console.log(data.toString());
+    });
+    console.log('Conection created...');
 })
 servidor.listen(PORT, function () {
-    console.log("CORRIENDO EN EL PUERTO " + PORT);
-    console.log("Esperando jugadores que busquen partida...");
+    console.log("Running on PORT " + PORT);
+    console.log("Waiting for conections...");
 })
